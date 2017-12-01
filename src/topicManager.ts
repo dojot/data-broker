939f4320965933ef6722b9620c9b0f36d0c1cd62
Matheus Magalhaes <matheusr@cpqd.com.br>
2017-12-01 17:58:21 -0200

    Implements remainder of topic management

--- src/topicManager.ts
+++ src/topicManager.ts
@@ -1,16 +1,14 @@
 /* jslint node: true */
 "use strict";
 
-import entities = require('./entities');
 import redis = require('redis');
-import {RedisManager} from './redisManager';
+import {ClientWrapper, RedisManager} from './redisManager';
 
-// var reval = require('redis-eval');
 var uuid = require('uuid/v4');
 
 // TODO this should also handle kafka ACL configuration
 export class TopicManager {
-  redis: redis.RedisClient
+  redis: ClientWrapper
   service: string
   get_set: string
 
@@ -38,14 +36,8 @@ export class TopicManager {
   getCreateTopic(subject: string, callback: (error: any, data: any) => any): void {
     try {
       const key: string = this.parseKey(subject);
-      console.log('key parsed', key);
       const tid: string = uuid();
-      console.log('will reval with', key, tid);
-      // reval(this.redis, this.get_set, [key], [uuid()], (err: any, data: any) => {
-      //   if (err) { callback(err, undefined); }
-      //   console.log(data);
-      //   callback(undefined, data);
-      // })
+      this.redis.runScript(this.get_set, [key], [tid], callback);
     } catch (e) {
       callback(e, undefined);
     }
